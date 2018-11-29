e49eb637398cde8ed515c268df3027df09a0667b
Joacim Dybedal <1422489+dybedal@users.noreply.github.com>
2018-11-29 12:45:53 +0100

    Added option to decompress Velodyne Points. Cleaned up main function. Fixed indentation.

--- src/main.cpp
+++ src/main.cpp
@@ -16,83 +16,80 @@
 
 void killHandler(int)
 {
-        ROS_INFO("%s","Shutdown request received.");
-        ros::NodeHandle nh;
-        ROS_INFO("%s","Terminating nodehandle.");
-        nh.shutdown();
-        ROS_INFO("%s","Terminating rosnode.");
-        ros::shutdown();
+  ROS_INFO("%s","Shutdown request received.");
+  ros::NodeHandle nh;
+  ROS_INFO("%s","Terminating nodehandle.");
+  nh.shutdown();
+  ROS_INFO("%s","Terminating rosnode.");
+  ros::shutdown();
 }
 
 int main(int argc, char **argv)
 {
-        // Initialize ROS
-        ros::init(argc, argv, "wp3_decompressor", ros::init_options::NoSigintHandler | ros::init_options::AnonymousName);
-        ros::NodeHandle nh("~");
+  // Initialize ROS
+  ros::init(argc, argv, "wp3_decompressor", ros::init_options::NoSigintHandler | ros::init_options::AnonymousName);
+  ros::NodeHandle nh("~");
 
-        signal(SIGINT, killHandler);
-        signal(SIGTERM, killHandler);
+  signal(SIGINT, killHandler);
+  signal(SIGTERM, killHandler);
 
-        ros::Rate loopRate(ROS_RATE);
+  ros::Rate loopRate(ROS_RATE);
 
-        // Multithreaded spinner
-//    	ros::AsyncSpinner spinner(8);
-//    	spinner.start();
-//    	usleep(1000000); // give the spinner some time to start (1000 ms)
+  // Multithreaded spinner
+  //    	ros::AsyncSpinner spinner(8);
+  //    	spinner.start();
+  //    	usleep(1000000); // give the spinner some time to start (1000 ms)
 
-        if(!nh.hasParam("sensor_name"))
-		    ROS_ERROR("%s","Missing _sensor_name:=<name> parameter! Shutting down...");
-        else if(!nh.hasParam("filter_value"))
-            ROS_ERROR("%s","Missing _filter_value:=<intensity> parameter! Shutting down...");
-        else{
-            
-            std::string sensorName;
-            nh.getParam("sensor_name", sensorName);
-            
-            int filterValue;
-            nh.getParam("filter_value", filterValue);
+  if(!nh.hasParam("sensor_name"))
+    ROS_ERROR("%s","Missing _sensor_name:=<name> parameter! Shutting down...");
+  else if(!nh.hasParam("filter_value"))
+    ROS_ERROR("%s","Missing _filter_value:=<intensity> parameter! Shutting down...");
+  else if(!nh.hasParam("sensor_type"))
+    ROS_ERROR("%s","Missing _sensor_type:=<0: Kinect, 1: Velodyne> parameter! Shutting down...");
+  else{
 
-            ros::Duration(1.0).sleep();
-
-            int sensorType = 0;
-            if (nh.hasParam("sensor_type"))
-                nh.getParam("sensor_type", sensorType);
-            
-            std::string outputTopic;
-            std::string inputTopic;
-            
-            switch(sensorType) {
-                
-                case 0: // Kinect
-                            
-                    outputTopic = "/master/"+sensorName+"/pc2_decompressed";
-                    inputTopic = "/"+sensorName+"/wp3/pc2_compressed";
-                    break;
-            
-                case 1: // Realsense
-                            
-                    outputTopic = "/master/"+sensorName+"/pc2_decompressed_rs";
-                    inputTopic = "/"+sensorName+"/wp3/pc2_compressed_rs";
-                    break;
-                    
-            } // End switch (sensorType)
-            
-            // Start decompressor
-            wp3::CloudDecompressor decompressor(outputTopic, inputTopic, filterValue, false);
-
-            
-            while(ros::ok()){
-                ros::spinOnce();
-                loopRate.sleep();
-            }
-         }
-
-        if(ros::ok()){
-                nh.shutdown();
-                ros::shutdown();
-        }
+    std::string outputTopic;
+    std::string inputTopic;
+    std::string sensorName;
+    int filterValue;
+    int sensorType;
 
 
-        ROS_INFO("%s","Shutdown complete.");
-        return 0;
+    nh.getParam("sensor_name", sensorName);
+    nh.getParam("filter_value", filterValue);
+    nh.getParam("sensor_type", sensorType);
+
+    switch(sensorType) {
+    case 0: // Kinect
+      outputTopic = "/master/"+sensorName+"/kinect_decomp";
+      inputTopic = "/"+sensorName+"/wp3/kinect_comp";
+      break;
+
+    case 1: // Velodyne
+      outputTopic = "/master/"+sensorName+"/velodyne_decomp";
+      inputTopic = "/"+sensorName+"/wp3/velodyne_comp";
+      break;
+
+    } // End switch (sensorType)
+
+    ros::Duration(1.0).sleep();
+
+    // Start decompressor
+    wp3::CloudDecompressor decompressor(outputTopic, inputTopic, filterValue, false);
+
+
+    while(ros::ok()){
+      ros::spinOnce();
+      loopRate.sleep();
+    }
+  }
+
+  if(ros::ok()){
+    nh.shutdown();
+    ros::shutdown();
+  }
+
+
+  ROS_INFO("%s","Shutdown complete.");
+  return 0;
 }
